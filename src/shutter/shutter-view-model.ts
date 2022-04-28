0f0c1dfef40f604228fbc0c0126916908a6c2a93
Pingoin <p.drente@gmx.de>
2022-04-28 20:39:04 +0200

    feat: change output on input change

--- src/shutter/shutter-view-model.ts
+++ src/shutter/shutter-view-model.ts
@@ -1,4 +1,5 @@
-import { Observable,fromObject } from '@nativescript/core'
+import { Observable,fromObject, GestureEventData } from '@nativescript/core'
+import { DataFormEventData } from 'nativescript-ui-dataform';
 import { ObservableProperty } from '../shared/observable-property-decorator';
 
 import { SelectedPageService } from '../shared/selected-page-service'
@@ -8,10 +9,12 @@ export class ShutterViewModel extends Observable {
     super()
 
     SelectedPageService.getInstance().updateSelectedPage('shutter')
-    setInterval(()=>{
+    /*setInterval(()=>{
         this.updateNpfValue();
-    },500);
+    },500);*/
+    console.log("Penis")
   }
+
   shutterData={
     focalLength:1200,
     pixelSize:23.6/6,
@@ -22,6 +25,10 @@ export class ShutterViewModel extends Observable {
   @ObservableProperty() npfValue:number=0;
 
   updateNpfValue(): void {
+
+  }
+  dfPropertyCommit(args:DataFormEventData):void{
+    console.log("Penis")
     let fNumber=this.shutterData.focalLength/this.shutterData.aperture;
     const value=(16.856*fNumber+0.0997*this.shutterData.focalLength+13.713*this.shutterData.pixelSize)/(this.shutterData.focalLength*Math.cos(this.shutterData.declination/180*Math.PI));
     this.set("npfValue",value);
